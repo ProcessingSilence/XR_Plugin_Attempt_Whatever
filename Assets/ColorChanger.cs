5a764c67e904bd4e26398401beaa469873f94d91
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-11 22:13:17 -0400

    10:13 PM

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Material greyMaterial;
+    public Material pinkMaterial;
+
+    public MeshRenderer _meshRenderer;
+    public XRGrabInteractable _grabInteractable;
+    
+    void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _grabInteractable = GetComponent<XRGrabInteractable>();
+        if (_meshRenderer != null)
+        {
+            Debug.Log("mesh renderer");
+        }
+        if (_grabInteractable != null)
+        {
+            Debug.Log("grab interactable");
+        }
+
+        _grabInteractable.onActivate.AddListener(SetPink);
+        _grabInteractable.onDeactivate.AddListener(SetGrey);
+    }
+
+    private void OnDestroy()
+    {
+        _grabInteractable.onActivate.RemoveListener(SetPink);
+        _grabInteractable.onDeactivate.RemoveListener(SetGrey);
+    }
+
+    private void SetGrey(XRBaseInteractor interactor)
+    {
+        _meshRenderer.material = greyMaterial;
+        Debug.Log("GREY");
+    }
+
+    private void SetPink(XRBaseInteractor interactor)
+    {
+        _meshRenderer.material = pinkMaterial;
+        Debug.Log("PINK");
+    }
+}
