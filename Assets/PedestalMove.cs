b9c3e583a186570a538356ae21297f6c15dcd56b
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-13 19:22:01 -0400

    7:21 PM

--- Assets/PedestalMove.cs
+++ Assets/PedestalMove.cs
@@ -6,15 +6,38 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class PedestalMove : MonoBehaviour
 {
     public XRSocketInteractor XrSocketInteractor_script;
-    // Start is called before the first frame update
-    void Start()
+
+    public float speed;
+    public float yDestroyZone;
+    
+    private bool alreadyMovingDown;
+
+    // Activated via XRSocketInteractor.cs 
+    public void StartMovingDown()
     {
-        
+        if (alreadyMovingDown == false)
+            StartCoroutine(Delay());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Delay()
     {
-        
+        if (alreadyMovingDown == false)
+        {
+            alreadyMovingDown = true;
+            XrSocketInteractor_script.enabled = false;
+            yield return new WaitForSecondsRealtime(0.5f);
+            StartCoroutine(MovingDown());
+        }
+    }
+    
+    IEnumerator MovingDown()
+    {
+        yield return new WaitForSecondsRealtime(0.01f);
+        transform.Translate(0,speed,0);
+        if (transform.position.y < yDestroyZone)
+        {
+            Destroy(gameObject);
+        }
+        StartCoroutine(MovingDown());
     }
 }
