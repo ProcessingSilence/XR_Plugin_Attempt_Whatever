43286d38837658e725d12df48b973f7ce7fe8c7d
ProcessingSilence <47760744+ProcessingSilence@users.noreply.github.com>
2020-09-14 14:34:35 -0400

    2:34 PM

--- Assets/Scripts/Explosive.cs
+++ Assets/Scripts/Explosive.cs
@@ -15,9 +15,15 @@ public class Explosive : MonoBehaviour
     private TrailRenderer _trailRenderer;
     
     private Rigidbody rb;
+
+    private ParticleSystem _particleSystem;
     
     public float explosiveRadius;
     public float force;
+
+    public float[] particleSizeRange;
+
+    public Light explosionLight;
 
 
     void Awake()
@@ -25,16 +31,24 @@ public class Explosive : MonoBehaviour
         _sphereCollider = GetComponent<SphereCollider>();
         rb = GetComponent<Rigidbody>();
         _trailRenderer = GetComponent<TrailRenderer>();
+        _particleSystem = GetComponent<ParticleSystem>();
+        explosionLight.intensity = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!startProcess)
+        {
+            explosionLight.intensity = transform.localScale.x;
+        }
+
         if (startProcess && startExplosion == false)
         {
             startExplosion = true;
             SetValues();
             StartCoroutine(Explosion());
+            ParticleExplosion();
         }
 
         if (destroyTrail)
@@ -86,5 +100,14 @@ public class Explosive : MonoBehaviour
     {
         float trailStart = _trailRenderer.startWidth;
         _trailRenderer.startWidth = Mathf.Lerp(trailStart, 0, Time.deltaTime * 5);
+        explosionLight.intensity = _trailRenderer.startWidth * 50;
+    }
+
+    void ParticleExplosion()
+    {
+        _particleSystem.startSize = Random.Range(particleSizeRange[0] * transform.localScale.x, particleSizeRange[1] * transform.localScale.x);
+        var particleSize = _particleSystem.shape;
+        particleSize.scale = Vector3.one * transform.localScale.x;
+        _particleSystem.Play();
     }
 }
